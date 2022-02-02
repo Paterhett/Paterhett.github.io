a929d79aca8231ea3be7c7e90a575cc08dbbdf14
Rhett Pate <you@example.com>
2022-02-02 11:34:53 -0500

    cards

--- Classwork/cards.js
+++ Classwork/cards.js
@@ -1,19 +1,20 @@
 let deck = [];
 for (let i = 2; i < 15; i++) {
-  deck.Push({
+  deck.push({
     rank: i,
     suit: "Hearts",
   });
-  deck.Push({
+  deck.push({
     rank: i,
     suit: "Spades",
   });
-  deck.Push({
+  deck.push({
     rank: i,
     suit: "Diamonds",
   });
-  deck.Push({
+  deck.push({
     rank: i,
     suit: "Clubs",
   });
 }
+console.log(deck);
